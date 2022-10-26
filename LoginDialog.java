c7e2b47a487c04d6bbe11eae79843b9278869b6b
Chae Won <winterrosy@gmail.com>
2022-10-26 13:08:58 +0900

    modified:loginDialog

--- LoginDialog.java
+++ LoginDialog.java
@@ -15,16 +15,24 @@ public class LoginDialog extends JDialog {
 	private SLogin sLogin;
 	
 	public LoginDialog(Frame parent) { 
-		super(parent, "LoginDialog", true); //modal
+		super(parent, "LoginDialog", false); //modal
 		this.setLocation(parent.getX()+30 , parent.getY()+30);
-		this.setSize(200,300);
+		this.setSize(430,300);
 		
-		//Box Layout : 라인세개가 y축으로 하나씩 붙게 하기
-		LayoutManager layoutManager1 = new FlowLayout();
-		LayoutManager layoutManager2 = new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS);
-		this.setLayout(layoutManager2);
+		LayoutManager layoutManager = new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS);
+		this.setLayout(layoutManager);
 		
+		//타이틀
+		JPanel Title = new JPanel();
+		Title.setBackground(Color.yellow);
+		this.add(Title);
+		JLabel titleLabel = new JLabel("수강신청 시스템");
+		titleLabel.setFont(new Font(null,Font.BOLD,25));
+		Title.add(titleLabel);
+		
+		//아이디
 		JPanel line1 = new JPanel();
+		line1.setBackground(Color.pink);
 		this.add(line1);
 		JLabel lbId = new JLabel("아이디: ");
 		JTextField tfId = new JTextField();
@@ -32,7 +40,9 @@ public class LoginDialog extends JDialog {
 		line1.add(lbId);
 		line1.add(tfId);
 		
+		//비밀번호
 		JPanel line2 = new JPanel();
+		line2.setBackground(Color.blue);
 		this.add(line2);
 		JLabel lbPassword = new JLabel("비밀번호: ");
 		JTextField tfPassword = new JTextField();
@@ -40,7 +50,9 @@ public class LoginDialog extends JDialog {
 		line2.add(lbPassword);
 		line2.add(tfPassword);
 		
+		//버튼
 		JPanel line3 = new JPanel();
+		line3.setBackground(Color.green);
 		this.add(line3);
 		JButton btLogin = new JButton("로그인");
 		line3.add(btLogin);
